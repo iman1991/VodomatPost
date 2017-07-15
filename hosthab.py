3f13c2fa6deb2283ac4f1f00afd426631e740b5a
LioHub <lioknuckles@gmail.com>
2017-07-15 14:33:07 +0300

    vodomat

--- hosthab.py
+++ hosthab.py
@@ -66,10 +66,14 @@ def connect(sock, addr):
                                 j = json.dumps(ypar)
                                 sock.send(j.encode("utf-8"))
 
-                    elif method == "Answer":
+                    elif method == "AnswerPay":
                             getBd=userbd.get_user(param['idT'])
                             getscore = int(getBd['score']) - int(param['score'])
                             hostbd.update_vodomatScore(param['idv'],getscore)
+                            userbd.update_user(**param)
+                            bot.send_message(param['idT'], "У вас на счету " + str(param['score']) + " рублей")
+
+                    elif method == "AnswerUP":
                             userbd.update_user(**param)
                             bot.send_message(param['idT'], "У вас на счету " + str(param['score']) + " рублей")
 
